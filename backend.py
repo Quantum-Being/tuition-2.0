a6a844132ec10b4c3e42c23dfe5c5116b8a81841
Quantum-Being <snipe.singularity@gmail.com>
2025-09-15 12:14:25 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,20 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", x=0)
 
 @app.route("/authorisation", methods=["POST"])
 def authorisation():
     un = request.form["username"]
     p = request.form["password"]
-    if un == "Co-Owner-One":
-        if p == "Quantum-Space":
-            return render_template("coone.html")
-            return render_template("home.html", x=0)
 
+    if un == "Co-Owner-One" and p == "Quantum-Space":
+        return render_template("coone.html")
     else:
         return render_template("home.html", x=1)
 
