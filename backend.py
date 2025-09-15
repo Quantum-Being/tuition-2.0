4faf320def8a5796a3a7885d9a840c340644fe33
Quantum-Being <snipe.singularity@gmail.com>
2025-09-15 17:36:14 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
@@ -11,7 +12,7 @@ def authorisation():
     un = request.form.get("username", "")
     p = request.form.get("password", "")
 
-    if un == "Co-Owner-One" and p == "Quantum-Space":
+    if un == "Co-Owner-One" and p == os.getenv("COOWNER_ONE_USER"):
         return render_template("coone.html")
 
     elif un == "Co-Owner-Two" and p == "AayushAayra":
